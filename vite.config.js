e9ba6bd9568ce0d6abc87b5a0a5fba425978b789
LcDevJunior <veryluckykurniawan@gmail.com>
2025-07-05 13:52:11 +0700

    ww

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Hangman-Game-React-js",
   plugins: [react(), tailwindcss()],
 });
